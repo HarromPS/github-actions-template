da77a208cdf6bbc5915cd45d8f70fe81c9caa07f
HarromPS <anirudhsharma0192837465@gmail.com>
2024-11-11 21:04:20 +0530

    demo

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello")
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+if(not os.path.exists("sample.txt")):
+    f = open("sample.txt",'x')
+    f.write("Hello\n")
+    f.close()
+else:
+    f = open("sample.txt",'a')
+    f.write("Hello\n")
+    f.close()
