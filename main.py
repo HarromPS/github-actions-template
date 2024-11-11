3491062eb596a31fe90053621058b134daaffbb8
HarromPS <anirudhsharma0192837465@gmail.com>
2024-11-11 21:36:07 +0530

    demo

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 from datetime import datetime
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-if not os.path.exists("sample.txt"):
-    with open("sample.txt", 'x') as f:
+if not os.path.exists("README.md"):
+    with open("README.md", 'x') as f:
         f.write("Hello\n")
 else:
-    with open("sample.txt", 'a') as f:
+    with open("README.md", 'a') as f:
         f.write(f"Hello {datetime.now()}\n")
