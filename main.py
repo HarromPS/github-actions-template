491abfa89bc1ae71753732831c06e724937ca0a7
HarromPS <anirudhsharma0192837465@gmail.com>
2024-11-14 18:30:42 +0530

    changed file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os
-from datetime import datetime
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# import os
+# from datetime import datetime
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-if not os.path.exists("README.md"):
-    with open("README.md", 'x') as f:
-        f.write("Hello\n")
-else:
-    with open("README.md", 'a') as f:
-        f.write(f"Hello {datetime.now()}\n")
+# if not os.path.exists("README.md"):
+#     with open("README.md", 'x') as f:
+#         f.write("Hello\n")
+# else:
+#     with open("README.md", 'a') as f:
+#         f.write(f"Hello {datetime.now()}\n")
